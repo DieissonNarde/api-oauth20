c0b1bb2a0636e9b6f72e0b1bc10421bfcaeb023e
DieissonNarde <dieissonnarde@gmail.com>
2023-02-19 21:53:53 -0400

    feat: change google config

--- configuration/config.js
+++ configuration/config.js
@@ -1,5 +1,7 @@
-module.exports={
-    "api_key": "yourKey.apps.googleusercontent.com",
-    "api_secret": "secretKey",
-    "callback_url": "http://localhost:3000/auth/google/callback"
-}
+require('dotenv').config();
+
+module.exports = {
+  api_key: process.env.GOOGLE_API_KEY,
+  api_secret: process.env.GOOGLE_SECRET_KEY,
+  callback_url: 'http://localhost:3000/auth/google/callback',
+};
